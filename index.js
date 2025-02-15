f7dbc3578b1adb4b403429f6670635a7e2a10fa6
Bruno Pereira <brunopereirabzrdev@gmail.com>
2025-02-15 15:53:27 -0300

    Estrutura basica

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import Handlebars from 'handlebars';
+import handlebars from 'express-handlebars';
+import bodyParser from 'body-parser';
+import { allowInsecurePrototypeAccess } from '@handlebars/allow-prototype-access';
+
+const app = express();
+const porta = 8000;
+
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+//CONFIGURAÇÃO DA PASTA ESTATICA
+const __filename = fileURLToPath(import.meta.url); // Obtem o nome do arquivo atual
+const __dirname = path.dirname(__filename); // Obtem o diretorio do arquivo atual
+
+/*CONFIGURAÇÃO DA VISÃO*/ // Define o layout padrão para todas as páginas
+app.engine('handlebars',handlebars.engine({
+    defaultLayout: 'principal', 
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}))
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+
+/*ROTAS DO SISTEMA*/
+
+//Rota inicio
+app.get('/', function(req, res){
+    res.render('admin/index');
+})
+
+app.listen(porta, function(){
+    console.log('Servidor rodando em http://localhost:' + porta);
+});
