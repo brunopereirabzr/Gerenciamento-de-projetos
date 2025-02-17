90f7043f30ec34d614721a22a00712a4a0c59631
Bruno Pereira <brunopereira@acad.ifma.edu.br>
2025-02-17 19:42:55 -0300

    Get para post router

--- routes/projeto.js
+++ routes/projeto.js
@@ -3,7 +3,11 @@ const router = express.Router()
 import ProjetoController from "../controller/ProjetoController.js"
 
 router.get('/', ProjetoController.index)
-router.get('/cadastrar', (req, res) => {res.render('produto/cadastrar')})
-router.get('/cadastrar', ProjetoController.cadastrar)
+
+router.get('/cadastrar', (req, res) => {
+    res.render('projeto/cadastrar')
+})
+
+router.post('/cadastrar', ProjetoController.cadastrar)
 
 export default router
