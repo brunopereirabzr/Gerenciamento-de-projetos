888a37d999b85c81d9baa15a169ae90d1266d62b
Bruno Pereira <brunopereirabzrdev@gmail.com>
2025-02-18 04:12:19 -0300

    Modelo comentário

---
+++ models/Comentario.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/banco.js"; 
+import Projeto from "./Projeto.js"; 
+
+const Comentario = sequelize.define("Comentario", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    descricao: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    projetoId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Projeto, 
+            key: "id"
+        },
+        onDelete: "CASCADE"
+    }
+}, {
+    tableName: "comentario",
+    timestamps: true
+});
+
+// Relacionamento entre Projetos e Tarefas
+Projeto.hasMany(Comentario, { foreignKey: "projetoId" });
+Comentario.belongsTo(Projeto, { foreignKey: "projetoId" });
+
+export default Comentario;
